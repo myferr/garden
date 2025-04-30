55d16bff2659fc25769f5cd54d3dc6e0b98799c8
MyferIsADev <contactme.myfer@protonmail.com>
2025-04-29 20:57:29 -0500

    old file, API url is not valid.'

--- deployment/lib/getAPI.ts
+++
@@ -1,9 +0,0 @@
-const API_URL = "https://gardenapi_lkjhgfdsw.pages.dev/";
-
-export function gardenAPI() {
-  const data = fetch(API_URL).then((res) => {
-    return res;
-  });
-
-  return data;
-}
