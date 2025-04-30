f6d9ca73d16845a7234cd52f28472fbaed6e5b9a
MyferIsADev <contactme.myfer@protonmail.com>
2025-04-30 13:42:56 -0500

    Update layout.tsx

--- deployment/app/layout.tsx
+++ deployment/app/layout.tsx
@@ -64,7 +64,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased dark`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased dark`} id="layout"
       >
         {children}
       </body>
